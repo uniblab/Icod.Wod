571f683c3e7964bb96119435b16e3228eb49cdb1
uniblab <uniblab@hotmail.com>
2017-06-26 14:13:37 -0400

    Adding the files
    Signed-off-by: uniblab <uniblab@hotmail.com>

---
+++ src/File/RmZip.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Icod.Wod.File {
+
+	[System.Serializable]
+	[System.Xml.Serialization.XmlType(
+		"rmZip",
+		Namespace = "http://Icod.Wod",
+		IncludeInSchema = true
+	)]
+	public sealed class RmZip : ZipOperationBase {
+
+		#region .ctor
+		public RmZip() : base() {
+		}
+		public RmZip( WorkOrder workOrder ) : base( workOrder ) {
+		}
+		#endregion .ctor
+
+
+		#region methods
+		public sealed override void DoWork( WorkOrder workOrder ) {
+			this.WorkOrder = workOrder ?? throw new System.ArgumentNullException( "workOrder" );
+			var sourceD = this.Source;
+			sourceD.WorkOrder = workOrder;
+			var source = sourceD.GetFileHandler( workOrder );
+
+			var handler = this.GetFileHandler( workOrder );
+			System.String file;
+			System.IO.Stream buffer;
+			System.Collections.Generic.IEnumerable<System.IO.Compression.ZipArchiveEntry> entries;
+			foreach ( var zipFile in handler.ListFiles().Where(
+				x => x.FileType.Equals( FileType.File )
+			) ) {
+				buffer = new System.IO.MemoryStream();
+				file = zipFile.File;
+				using ( var a = handler.OpenReader( file ) ) {
+					a.CopyTo( buffer );
+				}
+				buffer.Seek( 0, System.IO.SeekOrigin.Begin );
+				using ( var zipArchive = this.GetZipArchive( buffer, System.IO.Compression.ZipArchiveMode.Update ) ) {
+					entries = this.ListEntries( zipArchive, sourceD ).Reverse();
+					foreach ( var e in entries ) {
+						e.Delete();
+					}
+				}
+				buffer.Seek( 0, System.IO.SeekOrigin.Begin );
+				handler.Overwrite( buffer, file );
+			}
+		}
+		#endregion methods
+
+	}
+
+}
---
+++ src/File/ZipOperationBase.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+
+namespace Icod.Wod.File {
+
+	[System.Serializable]
+	[System.Xml.Serialization.XmlType(
+		"zipOperation",
+		Namespace = "http://Icod.Wod",
+		IncludeInSchema = true
+	)]
+	public abstract class ZipOperationBase : FileOperationBase {
+
+		#region fields
+		private System.String myCodePage;
+		private System.Boolean myTruncateEntryName;
+		#endregion fields
+
+
+		#region .ctor
+		protected ZipOperationBase() : base() {
+			myCodePage = "windows-1252";
+			myTruncateEntryName = true;
+		}
+		protected ZipOperationBase( WorkOrder workOrder ) : base( workOrder ) {
+			myCodePage = "windows-1252";
+			myTruncateEntryName = true;
+		}
+		#endregion .ctor
+
+
+		#region properties
+		[System.Xml.Serialization.XmlAttribute(
+			"codePage",
+			Namespace = "http://Icod.Wod"
+		)]
+		[System.ComponentModel.DefaultValue( "windows-1252" )]
+		public System.String CodePage {
+			get {
+				return myCodePage;
+			}
+			set {
+				myCodePage = value;
+			}
+		}
+
+		[System.Xml.Serialization.XmlAttribute(
+			"truncateEntryName",
+			Namespace = "http://Icod.Wod"
+		)]
+		[System.ComponentModel.DefaultValue( true )]
+		public System.Boolean TruncateEntryName {
+			get {
+				return myTruncateEntryName;
+			}
+			set {
+				myTruncateEntryName = value;
+			}
+		}
+
+		[System.Xml.Serialization.XmlElement(
+			"source",
+			Namespace = "http://Icod.Wod",
+			IsNullable = false
+		)]
+		public FileDescriptor Source {
+			get;
+			set;
+		}
+		#endregion properties
+
+
+		#region methods
+		public virtual System.Text.Encoding GetEncoding() {
+			var cp = this.CodePage;
+			System.Int32 cpNumber;
+			if ( System.Int32.TryParse( cp, out cpNumber ) ) {
+				return System.Text.Encoding.GetEncoding( cpNumber );
+			} else {
+				return System.Text.Encoding.GetEncoding( cp );
+			}
+		}
+
+		protected virtual System.Collections.Generic.IEnumerable<System.IO.Compression.ZipArchiveEntry> ListEntries( System.IO.Compression.ZipArchive zip, FileDescriptor source ) {
+			if ( null == zip ) {
+				throw new System.ArgumentNullException( "zip" );
+			} else if ( null == source ) {
+				throw new System.ArgumentNullException( "source" );
+			}
+
+			var regexPattern = source.RegexPattern;
+			var entries = zip.Entries;
+			var regexMatch = System.String.IsNullOrEmpty( regexPattern )
+				? entries
+				: entries.Where(
+					x => System.Text.RegularExpressions.Regex.IsMatch( x.FullName, regexPattern )
+				)
+			;
+			var path = source.ExpandedPath;
+			var dirMatch = System.String.IsNullOrEmpty( path )
+				? regexMatch
+				: regexMatch.Where(
+					x => x.FullName.StartsWith( path )
+				)
+			;
+			var name = source.ExpandedName;
+			return System.String.IsNullOrEmpty( name )
+				? dirMatch
+				: dirMatch.Where(
+					x => x.Name.Equals( name )
+				)
+			;
+		}
+
+		protected virtual System.IO.Compression.ZipArchive GetZipArchive( System.IO.Stream stream, System.IO.Compression.ZipArchiveMode zipArchiveMode ) {
+			if ( null == stream ) {
+				throw new System.ArgumentNullException( "stream" );
+			}
+			return new System.IO.Compression.ZipArchive( stream, zipArchiveMode, true, this.GetEncoding() );
+		}
+		#endregion methods
+
+	}
+
+}
