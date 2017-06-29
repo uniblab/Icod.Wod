c48edb538e7555011bdf2bf2dd26dd636e8ddb08
uniblab <uniblab@hotmail.com>
2017-06-29 00:22:45 -0400

    starting exec

---
+++ src/File/ExecuteFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Icod.Wod.File {
+
+	[System.Serializable]
+	[System.Xml.Serialization.XmlType(
+		"executeFile",
+		Namespace = "http://Icod.Wod",
+		IncludeInSchema = true
+	)]
+	public sealed class ExecuteFile : FileOperationBase {
+
+		#region fields
+		private System.String myStdErrCodePage;
+		private System.String myStdOutCodePage;
+		#endregion fields
+
+	}
+
+}
