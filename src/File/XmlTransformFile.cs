9553a9cee9597cf588d202323a6c8795ee9952ce
uniblab <tjb284@cornell.edu>
2019-01-12 01:00:07 -0500

    refactoring, so I don't have using ( ... ) { ... } blocks nested 20 levels deep
    Signed-off-by: uniblab <tjb284@cornell.edu>

--- src/File/XmlTransformFile.cs
+++ src/File/XmlTransformFile.cs
@@ -35,31 +35,32 @@ namespace Icod.Wod.File {
 		public sealed override void DoWork( WorkOrder workOrder, IStack<ContextRecord> context ) {
 			this.Context = context ?? Stack<ContextRecord>.Empty;
 			this.WorkOrder = workOrder ?? throw new System.ArgumentNullException( "workOrder" );
+			var source = this.GetFileHandler( workOrder ) ?? throw new System.InvalidOperationException();
 			this.Destination.WorkOrder = workOrder;
-			var dest = this.Destination.GetFileHandler( workOrder );
-			var source = this.GetFileHandler( workOrder );
-			var xslt = this.XsltFile.GetFileHandler( workOrder );
-			if ( ( null == dest ) || ( null == source ) || ( null == xslt ) ) {
-				throw new System.InvalidOperationException();
-			}
-			System.Xml.Xsl.XslCompiledTransform xform = new System.Xml.Xsl.XslCompiledTransform();
+			var dest = this.Destination.GetFileHandler( workOrder ) ?? throw new System.InvalidOperationException();
+			this.XsltFile.WorkOrder = workOrder;
+			var xslt = this.XsltFile.GetFileHandler( workOrder ) ?? throw new System.InvalidOperationException();
+
+			var xform = new System.Xml.Xsl.XslCompiledTransform();
 			using ( var xsltFileReader = xslt.OpenReader( xslt.ListFiles().First().File ) ) {
 				using ( var xslReader = System.Xml.XmlReader.Create( xsltFileReader ) ) {
 					xform.Load( xslReader );
 				}
 			}
-			var files = source.ListFiles().Select(
-				x => x.File
-			);
-			foreach ( var file in files ) {
-				using ( var fileReader = source.OpenReader( file ) ) {
-					using ( var xmlReader = System.Xml.XmlReader.Create( fileReader ) ) {
-						using ( var buffer = this.DoWork( xmlReader, xform ) ) {
-							var dfd = dest.FileDescriptor;
-							dest.Overwrite( buffer, dfd.GetFilePathName( dest, file ) );
-						}
-					}
+			foreach ( var file in source.ListFiles() ) {
+				this.DoWork( file, xform, dest );
+			}
+		}
+		private void DoWork( FileEntry source, System.Xml.Xsl.XslCompiledTransform xslTransform, FileHandlerBase destination ) {
+			using ( var fileReader = source.Handler.OpenReader( source.File ) ) {
+				using ( var xmlReader = System.Xml.XmlReader.Create( fileReader ) ) {
+					this.DoWork( xmlReader, xslTransform, destination, source.File );
 				}
+			}
+		}
+		private void DoWork( System.Xml.XmlReader source, System.Xml.Xsl.XslCompiledTransform xslTransform, FileHandlerBase destination, System.String filePathName ) {
+			using ( var buffer = this.DoWork( source, xslTransform ) ) {
+				destination.Overwrite( buffer, destination.FileDescriptor.GetFilePathName( destination, filePathName ) );
 			}
 		}
 		private System.IO.Stream DoWork( System.Xml.XmlReader source, System.Xml.Xsl.XslCompiledTransform xslTransform ) {
