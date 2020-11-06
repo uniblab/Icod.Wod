eb29f4483439d18acfa86a9edddbc482ac44b03a
uniblab <uniblab@hotmail.com>
2020-11-06 05:32:47 -0500

    updating assembly info copyright information
    Signed-off-by: uniblab <uniblab@hotmail.com>

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Icod.Wod" )]
-[assembly: AssemblyCopyright( "Copyright ?  2019" )]
+[assembly: AssemblyCopyright( "Copyright 2020, Timothy J. Bruce" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
