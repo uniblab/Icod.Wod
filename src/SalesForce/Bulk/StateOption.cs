fe46304b54f7d99f17a433002331291e5037c6a6
uniblab <uniblab@hotmail.com>
2023-12-08 03:30:56 -0500

    Feat: add new interface, operators, .ctor for working with Strings

--- src/SalesForce/Bulk/StateOption.cs
+++ src/SalesForce/Bulk/StateOption.cs
@@ -1,18 +1,25 @@
 // Copyright 2022, Timothy J. Bruce
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Security.Policy;
 
 namespace Icod.Wod.SalesForce.Bulk {
 
 	[System.Serializable]
-	public sealed class StateOption : System.IEquatable<StateOption> {
+	public sealed class StateOption : System.IEquatable<StateOption>, System.IEquatable<System.String> {
 
 		#region fields
+		private const System.String theAbortedString = "Aborted";
 		private static readonly StateOption theAborted;
+		private const System.String theFailedString = "Failed";
 		private static readonly StateOption theFailed;
+		private const System.String theInProgressString = "InProgress";
 		private static readonly StateOption theInProgress;
+		private const System.String theJobCompleteString = "JobComplete";
 		private static readonly StateOption theJobComplete;
+		private const System.String theOpenString = "Open";
 		private static readonly StateOption theOpen;
+		private const System.String theUploadCompleteString = "UploadComplete";
 		private static readonly StateOption theUploadComplete;
 
 		private readonly System.String myValue;
@@ -23,15 +30,17 @@ namespace Icod.Wod.SalesForce.Bulk {
 
 		#region .ctor
 		static StateOption() {
-			theAborted = new StateOption( "Aborted", "Aborted" );
-			theFailed = new StateOption( "Failed", "Failed" );
-			theInProgress = new StateOption( "InProgress", "InProgress" );
-			theJobComplete = new StateOption( "JobComplete", "JobComplete" );
-			theOpen = new StateOption( "Open", "Open" );
-			theUploadComplete = new StateOption( "UploadComplete", "UploadComplete" );
+			theAborted = new StateOption( theAbortedString );
+			theFailed = new StateOption( theFailedString );
+			theInProgress = new StateOption( theInProgressString );
+			theJobComplete = new StateOption( theJobCompleteString );
+			theOpen = new StateOption( theOpenString );
+			theUploadComplete = new StateOption( theUploadCompleteString );
 		}
 
-		private StateOption( System.String value, System.String name ) {
+		private StateOption( System.String value ) : this( value, value ) {
+		}
+		private StateOption( System.String value, System.String name ) : base() {
 			if ( System.String.IsNullOrEmpty( value ) ) {
 				throw new System.ArgumentNullException( "value" );
 			} else if ( System.String.IsNullOrEmpty( name ) ) {
@@ -111,6 +120,9 @@ namespace Icod.Wod.SalesForce.Bulk {
 				return this.Equals( ( obj as StateOption ) );
 			}
 		}
+		public System.Boolean Equals( System.String other ) {
+			return this.Value.Equals( other, System.StringComparison.OrdinalIgnoreCase );
+		}
 		public sealed override System.Int32 GetHashCode() {
 			return myHashcode;
 		}
@@ -150,6 +162,23 @@ namespace Icod.Wod.SalesForce.Bulk {
 			}
 		}
 		public static System.Boolean operator !=( StateOption left, StateOption right ) {
+			return !( left == right );
+		}
+
+		public static explicit operator System.String( StateOption value ) { 
+			return value?.Value;
+		}
+
+		public static System.Boolean operator ==( StateOption left, System.String right ) {
+			if ( ( left is null ) && ( right is null ) ) {
+				return true;
+			} else if ( ( left is null ) || ( right is null ) ) {
+				return false;
+			} else {
+				return left.Equals( right );
+			}
+		}
+		public static System.Boolean operator !=( StateOption left, System.String right ) {
 			return !( left == right );
 		}
 		#endregion static methods
