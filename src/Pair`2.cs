84b48cc49811c5affa53d689c911673942ee555f
uniblab <uniblab@hotmail.com>
2023-12-09 17:30:47 -0500

    improve relational operators.

--- src/Pair`2.cs
+++ src/Pair`2.cs
@@ -3,18 +3,35 @@
 namespace Icod.Wod {
 
 	[System.Serializable]
-	public sealed class Pair<S, T> {
+	public sealed class Pair<S, T> : System.IEquatable<Pair<S, T>> {
 
 		#region fields
+		private static readonly System.Int32 theHashCode;
+
 		private readonly S myFirst;
 		private readonly T mySecond;
+		private readonly System.Int32 myHashCode;
 		#endregion  fields
 
 
 		#region .ctor
+		static Pair() {
+			theHashCode = typeof( Pair<S, T> ).GetHashCode();
+		}
 		public Pair( S first, T second ) : base() {
 			myFirst = first;
 			mySecond = second;
+			myHashCode = theHashCode;
+			if ( myFirst is object ) {
+				unchecked {
+					myHashCode += myFirst.GetHashCode();
+				}
+			}
+			if ( mySecond is object ) {
+				unchecked {
+					myHashCode += mySecond.GetHashCode();
+				}
+			}
 		}
 		#endregion .ctor
 
@@ -32,6 +49,29 @@ namespace Icod.Wod {
 		}
 		#endregion properties
 
+		#region methods
+		public sealed override System.Int32 GetHashCode() {
+			return myHashCode;
+		}
+		public sealed override System.Boolean Equals( System.Object obj ) {
+			if ( obj is null ) {
+				return false;
+			} else if ( ReferenceEquals( this, obj ) ) {
+				return true;
+			} else {
+				return this.Equals( obj as Pair<S, T> );
+			}
+		}
+		public System.Boolean Equals( Pair<S, T> other ) {
+			if ( other is null ) {
+				return false;
+			} else if ( ReferenceEquals( this, other ) ) {
+				return true;
+			} else {
+				return Equals( myFirst, other.First ) && Equals( mySecond, other.Second );
+			}
+		}
+		#endregion methods
 	}
 
 }
